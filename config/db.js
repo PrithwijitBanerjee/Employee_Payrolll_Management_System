949feca4c4a3c0a1b0be3ec6ecc571e87c4f0cdb
Sumon Porel <sumonporel2003@gmail.com>
2025-09-24 11:25:21 +0530

    log issue solved

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('employeeManagement', 'postgres', 'root', {
   host: 'localhost',
   dialect: 'postgres',
-  logging: console.log
+  logging: false
 });
 
 module.exports = sequelize;
